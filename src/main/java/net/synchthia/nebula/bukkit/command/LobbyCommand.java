1c122862b7edd6c41447cb7533b29e753fe83d1c
Laica Lunasys <laica.lunasys@gmail.com>
2018-01-04 13:57:45 +0900

    Fixed Command Problem when Lobby server offline

--- src/main/java/net/synchthia/nebula/bukkit/command/LobbyCommand.java
+++ src/main/java/net/synchthia/nebula/bukkit/command/LobbyCommand.java
@@ -3,10 +3,14 @@ package net.synchthia.nebula.bukkit.command;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
+import net.synchthia.api.nebula.NebulaProtos;
 import net.synchthia.nebula.bukkit.server.ServerAPI;
 import net.synchthia.nebula.bukkit.server.ServerAction;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import java.util.Map;
 
 /**
  * @author Laica-Lunasys
@@ -15,6 +19,11 @@ public class LobbyCommand extends BaseCommand {
     @CommandAlias("lobby|hub")
     @CommandPermission("nebula.command.lobby")
     public void onLobby(CommandSender sender) {
-        ServerAction.connect(Bukkit.getPlayer(sender.getName()), ServerAPI.determinateLobby().getValue().getName());
+        Map.Entry<String, NebulaProtos.ServerEntry> lobby = ServerAPI.determinateLobby();
+        if (lobby == null) {
+            sender.sendMessage(ChatColor.RED + "All Fallback Server is down! Type /quit for Disconnect.");
+        } else {
+            ServerAction.connect(Bukkit.getPlayer(sender.getName()), lobby.getValue().getName());
+        }
     }
 }
